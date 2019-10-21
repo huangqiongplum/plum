744f04e908798f39832e58a94ef4b818c9cc3cf1
huangqiongplum <55393503+huangqiongplum@users.noreply.github.com>
2019-10-21 10:53:26 +0800

    Add files via upload

---
+++ student.cpp
@@ -0,0 +1,11 @@
+﻿1>------ 已启动生成: 项目: student, 配置: Debug Win32 ------
+1>student.cpp
+1>d:\c++\student\student\student.cpp(79): warning C4102: “case7”: 未引用的标签
+1>d:\c++\student\student\student.cpp(89): error C2065: “student；”: 未声明的标识符
+1>d:\c++\student\student\student.cpp(89): error C2146: 语法错误: 缺少“;”(在标识符“test”的前面)
+1>d:\c++\student\student\student.cpp(89): error C2065: “test”: 未声明的标识符
+1>d:\c++\student\student\student.cpp(91): error C2065: “test”: 未声明的标识符
+1>d:\c++\student\student\student.cpp(93): error C2065: “test”: 未声明的标识符
+1>d:\c++\student\student\student.cpp(95): error C2065: “test”: 未声明的标识符
+1>已完成生成项目“student.vcxproj”的操作 - 失败。
+========== 生成: 成功 0 个，失败 1 个，最新 0 个，跳过 0 个 ==========
